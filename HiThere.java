7c89a6036a762d98e5384432e0ceec54ea63b525
YanK123 <45793531+YanK123@users.noreply.github.com>
2018-12-11 16:51:25 +0200

    Add files via upload

---
+++ HiThere.java
@@ -0,0 +1,53 @@
+package yo;
+
+public class HiThere {
+    public static void main (String[] args) {
+        String a1 = "__________";
+        String a2 = "\u2761\u2761\u27611";
+        String a3 = "___";
+        String a43 = "____";
+        String a4 = "\u2761\u2761\u2761";
+        String a5 = "___________";
+        String a6 = "_____________";
+        String a7 = "_______________";
+        String a8 = "________________";
+        String b1 = "11";
+        String b2 = "111";
+        System.out.println (a1 + a2 + a3 + a4 + a43 + a2 + a1 + a3 + "__");
+        System.out.println (a5 + a2 + a43 + a4 + a43 + a2 + a1 + "___");
+        System.out.println (a6 + a2 + a43 + a4 + a43 + a2 + a1 + "_");
+        System.out.println (a7 + a2 + a43 + a4 + a43 + a2 + a43 + a43 + "_");
+        System.out.println (a8 + a2 + a43 + a4 + a43 + a2 + a43 + a43);
+        System.out.println (a8 + a2 + a43 + a4 + a43 + a2 + a43 + a43);
+        System.out.println (a7 + "__" + a2 + a43 + a4 + a43 + a2 + a43 + a3);
+        System.out.println (a6 + "___" + a2 + a43 + a4 + a43 + a2 + a43 + a43);
+        System.out.println (a7 + b1 + a43 + b2 + a43 + a3 + b1 + a1);
+        System.out.println (a1 + a4 + a4 + a4 + a4 + a4 + a4 + a4 + a4 + a4 + a4 + a3);
+        System.out.println("1" + a4 +a4 + a4 + a4 + a3 + a4 + a4 +a4 +a4 +a4 +a4 +a4 +a4 + a3);
+        System.out.println("1" + a4 +a4 + a4 + a4 + a3 + "1" + a4 + a4 +a4 +a4 +a4 +a4 +a4 + "\u2761\u2761" + a3);
+        System.out.println("1" + "\u2761\u2761" + a43 + a43 + "\u2761\u2761" + a3 + "1" + a4 + a4 +a4 +a4 +a4 +a4 +a4 +
+                "\u2761\u2761" + a3);
+        System.out.println("1" + "\u2761\u2761" + a43 + a43 + "\u2761\u2761" + a3 + "1" + a4 + a4 +a4 +a4 +a4 +a4 +a4 +
+                "\u2761\u2761" + a3);
+        System.out.println("1" + "\u2761\u2761" + a43 + a43 + "\u2761\u2761" + a3 + "\u2761" + a4 + a4 +a4 +a4 +a4 + a4
+                + a4 + "\u2761\u2761" + a3);
+        System.out.println("1" + "\u2761\u2761" + a43 + a43 + "\u2761\u2761" + a3 + "1" + a4 + a4 +a4 +a4 +a4 +a4 +a4 +
+                "\u2761\u2761" + a3);
+        System.out.println("_" + a4 + a4 + a4 + "\u2761\u2761\u2761" + "___" + a4 + a4 + a4 + a4 + a4 + a4 + a4 + a4 +
+                a3);
+        System.out.println("_" + a4 + a4 + a4 + "\u2761\u2761\u2761" + "___" + a4 + a4 + a4 + a4 + a4 + a4 + a4 + a4 +
+                a3);
+        System.out.println(a5 + "\u2761\u2761" + a43 + "1" + a4 + a4 + a4 + a4 + a4 + a4 + a4 + "1" + a3);
+        System.out.println(a5 + "1" + "\u2761\u2761" + a43  + a4 + a4 + a4 + a4 + a4 + a4 + a4  + a43);
+        System.out.println(a5 + a3  + a4 + a4 + a4 + a4 + a4 + a4 + a4 + a4 + "1" + "1" + a3);
+        System.out.println ("1" + "1" + a5 + a5 + a5 + a43 + "_" + "1" + "1"+ "1");
+        System.out.println ("1" + a4 + a4 + a4 + a4 + a4 + a4 + a4 + a4 + a4 + a4 + a4 + a4 + a4 + "111");
+        System.out.println (a3 + "\u2761\u2761" + "111111111" + a4 + a4 + a4 + a4 + a4 + a4 + "11111111" + "\u2761" +
+                "__");
+
+
+
+//
+
+    }
+    }
